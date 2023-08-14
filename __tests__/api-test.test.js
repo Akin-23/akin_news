54100cd74d3623abce61104a9b78bc7545a83911
Akin-23 <119421260+Akin-23@users.noreply.github.com>
2023-08-14 15:46:17 +0100

    question 3 complete

--- __tests__/api-test.test.js
+++ __tests__/api-test.test.js
@@ -2,7 +2,8 @@ const request = require("supertest");
 const db = require("../db/connection.js");
 const seed = require("../db/seeds/seed");
 const data = require("../db/data/test-data/index.js");
-const app = require('../app.js');
+const app = require("../app.js");
+const fs = require("fs/promises");
 
 beforeEach(() => {
   return seed(data);
@@ -12,7 +13,8 @@ afterAll(() => db.end());
 
 describe("/api/topics", () => {
   test("GET:200 sends an array of topics to the client", () => {
-    return request(app).get("/api/topics")
+    return request(app)
+      .get("/api/topics")
       .expect(200)
       .then(({ body }) => {
         const { topics } = body;
@@ -26,3 +28,26 @@ describe("/api/topics", () => {
   });
 });
 
+describe("/api", () => {
+  test("GET:200 sends object describing all the available endpoints in API", () => {
+    return request(app)
+      .get("/api")
+      .expect(200)
+      .then(({ body }) => {
+        let count = 0;
+        for (const endpoint in body) {
+          count++;
+          expect(body[endpoint]).toHaveProperty(
+            "description",
+            expect.any(String)
+          );
+          expect(body[endpoint]).toHaveProperty("queries", expect.any(Array));
+          expect(body[endpoint]).toHaveProperty(
+            "exampleResponse",
+            expect.any(Object)
+          );
+        }
+        expect(Object.keys(body).length).toBe(count);
+      });
+  });
+});
--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 const { getTopics } = require("./controllers/topics-controller");
+const { getEndpoints } = require("./controllers/endpoints-controller");
+
 
 app.get("/api/topics", getTopics);
+
+app.get("/api", getEndpoints)
 
 
 
